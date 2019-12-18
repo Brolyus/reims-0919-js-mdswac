f56cd8b2207c5f9f429ce80081406681d9bdf526
Brolyus <desousanthony@gmail.com>
2019-12-18 14:45:37 +0100

    we added the md to html converter module, tested and it works

--- mdswac/src/App.js
+++ mdswac/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import convertToHTML from 'markdown-to-html-converter'
+
+const markdownStr = '#We test MDSWAC app'
+const htmlStr = convertToHTML(markdownStr)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>{htmlStr}</header>
+        </div>
+    )
 }
 
-export default App;
+export default App
