8da2cb9e770bac7a3084ae11ba259efb1d0e15f8
Brolyus <desousanthony@gmail.com>
2019-12-19 10:55:02 +0100

    transformed input.js in function component instead of class

--- mdswac/src/components/Input.js
+++ mdswac/src/components/Input.js
@@ -2,47 +2,23 @@ import React, { Component } from 'react'
 import './input.css'
 import convertToHTML from 'markdown-to-html-converter'
 
-class Input extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      mdText: ''
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleKeyDown = this.handleKeyDown.bind(this)
-  }
-
-  handleKeyDown = e => {
-    if (e.key === 'Enter') {
-      console.log('yo')
-      return this.handleChange
-    }
-  }
-
-  handleChange(event) {
-    this.setState({ mdText: event.target.value })
-  }
-
-  render() {
-    let markdownStr = this.state.mdText
-    const htmlStr = convertToHTML(markdownStr)
-    return (
-      <div className='inputdiv'>
-        <label className='label' htmlFor='mdText'>
-          Saisir votre texte en markdown :
-        </label>
-        <textarea
-          className='area'
-          id='mdText'
-          placeholder='Saisir votre texte ici'
-          type='text'
-          value={this.props.mdText}
-          onChange={this.props.handleChange}
-          onKeyDown={this.props.handleKeyDown}
-        />
-      </div>
-    )
-  }
+const Input = props => {
+  return (
+    <div className='inputdiv'>
+      <label className='label' htmlFor='mdText'>
+        Saisir votre texte en markdown :
+      </label>
+      <textarea
+        className='area'
+        id='mdText'
+        placeholder='Saisir votre texte ici'
+        type='text'
+        value={props.mdText}
+        onChange={props.handleChange}
+        onKeyDown={props.handleKeyDown}
+      />
+    </div>
+  )
 }
 
 export default Input
