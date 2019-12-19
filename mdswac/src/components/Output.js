b1ba3e55744477b056cb2e3fafdd67dc11ae313b
Brolyus <desousanthony@gmail.com>
2019-12-20 00:01:13 +0100

    corrected some few compile error, need to fix last ones about buttons in a next patch

--- mdswac/src/components/Output.js
+++ mdswac/src/components/Output.js
@@ -2,7 +2,14 @@ import React from 'react'
 import './Output.css'
 
 const Output = props => {
-  return <code className='outputdiv'>{props.renderedText}</code>
+  return (
+    <code className='outputdiv'>
+      <label className='label' htmlFor='mdText'>
+        Voici votre texte en HTML :
+      </label>
+      <div className='areaoutput'>{props.renderedText}</div>
+    </code>
+  )
 }
 
 export default Output
